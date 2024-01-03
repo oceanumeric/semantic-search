7d581680b68b909eacd3f87adf6ab1251de8d368
oceanumeric <numerical.ocean@gmail.com>
2024-01-03 16:11:00 +0100

    [update]

--- client/src/lib/components/contactsTable.svelte
+++ client/src/lib/components/contactsTable.svelte
@@ -50,8 +50,10 @@
 						{contact.job}
 					</td>
 					<td class="px-6 py-4">
-						<input type="hidden" name="id" value={contact.id} />
-						<button type="submit" class="text-white bg-pink-700 hover:bg-pink-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Delete</button>
+						<form method="POST" action="?/delete" use:enhance>
+							<input type="hidden" name="id" value={contact.id} />
+							<button type="submit" class="text-white bg-pink-700 hover:bg-pink-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Delete</button>
+						</form>
 					</td>
 				</tr>
 			{/each}
